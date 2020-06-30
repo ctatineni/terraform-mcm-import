e76e03bc0c16bb3bfbac0514e58c8606d0f7749d
chandhubabu <chandhut@gmail.com>
2020-06-30 12:26:13 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,40 +16,32 @@ variable "helper_private_ip" {
 
 variable "private_registry_enabled" {
   type    = string
-  default = "false"
 }
 
 variable "docker_username" {
   type    = string
-  default = "null"
 }
 
 variable "docker_password" {
   type    = string
-  default = "null"
 }
 
 variable "imageRegistry" {
   type    = string
-  default = "null"
 }
 
 variable "mcm_hub_url" {
   type    = string
-  default = "null"
 }
 
 variable "mcm_username" {
   type    = string
-  default = "null"
 }
 
 variable "mcm_password" {
   type    = string
-  default = "null"
 }
 
 variable "cluster_id" {
   type    = string
-  default = "null"
 }
